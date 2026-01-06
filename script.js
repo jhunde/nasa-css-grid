72d54abb09d2addfb1f5979aa263e45e91324d19
hunde <jena.kelbessa@gmail.com>
2026-01-05 23:58:42 -0800

    testing fetch-api

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 require('dotenv').config()
 
 const API_KEY = process.env.API_KEY;
-const APOD_ULR = 'https://api.nasa.gov/planetary/apod?api_key=${API_KEY}';
+const APOD_ULR = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+async function fetchApod(params) {
+    try {
+        const response = await fetch(APOD_ULR);
+        if (!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);  // for debugging
+
+    }
+    catch(error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchApod();
